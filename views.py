0f6a64b1cf0ea9bb388c110b79c73f4b07936250
Riya Diwakar Nikhade <81016720+RiyaDN12@users.noreply.github.com>
2021-04-12 13:05:17 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def show(request):
+    return HttpResponse('Hello, This is Riya Nikhade of T2 batch and my PRN is 1841036')
